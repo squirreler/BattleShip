fccbce9d8e312582e462db40c498d0dc7368e954
Daniel B <dbbemailme@gmail.com>
2024-09-05 13:10:00 -0700

    Started building out the ship class and draggable functionality

--- index.js
+++ index.js
@@ -16,12 +16,13 @@ function createAndAppendTextElement(parent, elementString, text, id, classList) 
     return textElement;
 }
 
-function createGrid(parentDiv, tailwindOutlineColorString, styleList) {
+function createGrid(parentDiv, tailwindOutlineColorString, backgroundColor, styleList) {
     for (let i = 0; i < 100; i++) { // Get rid of async
         let screenDiv = document.createElement("div");
         screenDiv.id = `${ Math.trunc(i / 10)}-${i % 10}` //Theres no integer truncation for some reason...
         screenDiv.classList.add(...styleList);
         screenDiv.classList.add("outline", tailwindOutlineColorString);
+        screenDiv.classList.add(backgroundColor);
         if (i % (squaresPerScreen / 10) === 0) {
             screenDiv.classList.add("rounded-l-sm");
         }
@@ -32,10 +33,10 @@ function createGrid(parentDiv, tailwindOutlineColorString, styleList) {
         
     }
 }
-function createScreen(parentDiv, color, outlineColor, id, styleList) {
+function createScreen(parentDiv,outlineColor, id, styleList) {
     let screen = document.createElement("div");
     screen.classList.add(...styleList); // I love javascript
-    screen.classList.add(color, outlineColor);
+    screen.classList.add(outlineColor);
     parentDiv.appendChild(screen);
     return screen;
 }
@@ -56,61 +57,133 @@ function createButtonAndEventListener(parent, name, id, styleList, backgroundCol
 
 // let playerScreen = document.getElementById("player-screen");
 let gameScreenSection = document.getElementById("game-screens");
+const gameScreenSectionStyleList = ["flex", "flex-col", "nowrap", "items-center"];
+
+
+
+let gameInfoPanel = document.getElementById("game-info");
+
+let playerScreenSection = null;
+let computerScreenSection = null;
+const screenSectionStyleList = ["flex", "flex-col", "nowrap", "items-center"];
+
 let shipSelectionSection = document.getElementById("ship-selection");
 
 let playerScreen = null;
 let computerScreen = null;
-
-let playerScreenSection = null;
-let computerScreenSection = null;
-
-let shipSelectionScreen = null;
-
-let rotateButton = null;
-let startButton = null;
-
-const gameScreenSectionStyleList = ["flex", "flex-col", "nowrap", "items-center"];
-
-const screenSectionHeadingTextStyleList = ["py-4"];
-
 const screenLengthWidthSizePx = 500;
 const screenStyleList = ["flex", "flex-wrap", `w-[${screenLengthWidthSizePx}px]`, `h-[${screenLengthWidthSizePx}px]`, "bg-sky-500", "rounded-lg", "outline", "outline-8"];
 const squaresPerScreen = 100;
-
 const gridSquareLengthWidthSizePx = screenLengthWidthSizePx / squaresPerScreen * 10;
 const gridDivStyleList = [`w-[${gridSquareLengthWidthSizePx}px]`, `h-[${gridSquareLengthWidthSizePx}px]`]; 
 
 
+let shipSelectionSectionGrouping = null;
+let shipSelectionSectionGroupingStyleList = ["flex", "flex-row", "gap-x-5"];
 
-const shipSelectionTextStyleList = ["py-1"];
 
-const shipSelectionSectionStyleList = ["flex", "flex-row", "justify-center", "items-center","py-4", "w-[40vw]", "h-[20vh]", "min-w-[600px]", "min-h-[190px]", "bg-cyan-200", "outline", "outline-4", "outline-cyan-600", "rounded-lg"]
 
-const shipSelectionButtonDivStyleList = ["flex", "flex-row", "justify-center", "items-center", "px-4", "my-1", "py-3", "bg-cyan-200", "rounded-lg"];
+let shipSelectionScreen = null;
+const shipSelectionSectionStyleList = ["flex", "flex-row", "justify-center", "items-center","py-4", "w-[550px]", "h-[230px]", "min-w-[300px]", "bg-cyan-200", "outline", "outline-4", "outline-cyan-600", "rounded-lg"]
 
+let shipSelectionButtonDiv = null;
+const shipSelectionButtonDivStyleList = ["flex", "flex-col", "justify-center", "items-center", "px-4", "my-1", "py-3", "bg-cyan-200", "rounded-lg"];
+
+
+let rotateButton = null;
+let startButton = null;
 const shipSelectionButtonStyleList = ["flex", "px-4", "py-2", "mx-2", "my-2", "rounded-full"];
 
 
+const screenSectionHeadingTextStyleList = ["py-4"];
 
-playerScreenSection = createAndAppendChildElement(gameScreenSection, "section", "player-screen-section", gameScreenSectionStyleList);
-computerScreenSection = createAndAppendChildElement(gameScreenSection, "section", "computer-screen-section", gameScreenSectionStyleList);
+
+const shipSelectionTextStyleList = ["py-1"];
+
+
+
+
+
+
+
+playerScreenSection = createAndAppendChildElement(gameScreenSection, "section", "player-screen-section", screenSectionStyleList);
+computerScreenSection = createAndAppendChildElement(gameScreenSection, "section", "computer-screen-section", screenSectionStyleList);
 createAndAppendTextElement(playerScreenSection, "h1", "Your Ships", "", screenSectionHeadingTextStyleList);
 createAndAppendTextElement(computerScreenSection, "h1", "Computers Ships", "", screenSectionHeadingTextStyleList);
 
 
-playerScreen = createScreen(playerScreenSection, "bg-sky-500", "outline-blue-700", "player-screen", screenStyleList);
-computerScreen = createScreen(computerScreenSection, "bg-blue-500", "outline-red-500","player-screen", screenStyleList);
-createGrid(playerScreen, "outline-blue-700", gridDivStyleList);
-createGrid(computerScreen, "outline-red-500", gridDivStyleList); // set bg here
+playerScreen = createScreen(playerScreenSection, "outline-blue-700", "player-screen", screenStyleList);
+computerScreen = createScreen(computerScreenSection, "outline-red-500","player-screen", screenStyleList);
+createGrid(playerScreen, "outline-blue-700","bg-sky-500",gridDivStyleList);
+createGrid(computerScreen, "outline-rose-500", "bg-sky-500", gridDivStyleList); // set bg here
+
+
 
 createAndAppendTextElement(shipSelectionSection, "h1", "Choose Ships", "", shipSelectionTextStyleList);
-shipSelectionScreen = createAndAppendChildElement(shipSelectionSection, "div", "ship-selection-screen", shipSelectionSectionStyleList);
+shipSelectionSectionGrouping = createAndAppendChildElement(shipSelectionSection, "div", "ship-selection-grouping", shipSelectionSectionGroupingStyleList);
+shipSelectionScreen = createAndAppendChildElement(shipSelectionSectionGrouping, "div", "ship-selection-screen", shipSelectionSectionStyleList);
 
-createAndAppendChildElement(shipSelectionSection, "div", "ship-selection-buttons",shipSelectionButtonDivStyleList);
+shipSelectionButtonDiv = createAndAppendChildElement(shipSelectionSectionGrouping, "div", "ship-selection-buttons",shipSelectionButtonDivStyleList);
 
 //Tune colors later
-rotateButton = createButtonAndEventListener(shipSelectionSection.lastChild, "rotate", "rotate-button", shipSelectionButtonStyleList, "bg-red-500", () => {console.log("Button Clicked")});
-startButton = createButtonAndEventListener(shipSelectionSection.lastChild, "submit", "submit-button", shipSelectionButtonStyleList, "bg-blue-500", () => {console.log("Button Clicked")});
+rotateButton = createButtonAndEventListener(shipSelectionButtonDiv, "rotate", "rotate-button", shipSelectionButtonStyleList, "bg-red-500", () => {console.log("Button Clicked")});
+rotateButton.draggable = true;
+console.dir(rotateButton);
+startButton = createButtonAndEventListener(shipSelectionButtonDiv, "submit", "submit-button", shipSelectionButtonStyleList, "bg-blue-500", () => {console.log("Button Clicked")});
+
+
+rotateButton.addEventListener("mouseUp", (event) => { //use window.x when you use this for ship //make ship.style.display false; trhough minipulating classelist when drag start
+    console.log(event);
+    rotateButton.classList.add("hidden"); // Drag and drop api is overkill for my uses, just use event listeners to make the thing draggable. 
+});
+
+//Your priority is to build out corrdinate system tracking.
+class ship {
+    length;   // 2-5
+    rotation; // - |
+    health; // [o, o, x, o, x]
+    topXCoord;
+    topYCoord;
+    constructor(length, rotation, color) {
+        this.length = length;
+        this.rotation = rotation;
+        this.color = color;
+        this.topXCoord = null;
+        this.topYCOord = null;
+        this.health = () => {
+            let healthArray = [];
+            for (let i = 0; i < this.length;i++) {this.health.push("o");};
+            return healthArray;
+        };
+        console.log(this.health);
+        
+
+    }
+    placeToGrid(x, y, rotation) {
+        this.topXCoord = x;
+        this.topYCoord = y;
+        this.rotation = rotation;
+    }
+    render() {
+        
+    }
+    get isDestroyed() {
+        let isDestroyed = true;
+        for (sectionStatus of health) {
+            if (sectionStatus === "o") {
+                return false;
+            }
+        }
+        return this.isDestroyed;
+    }
+    // set health() {
+    //     return this.health;            //Need to work more on placing ships before I can confidently draw up the structure of this class. 
+    // }
+    // [o, o, o, x]
+    //isDead
+    //etc
+
+}
 
 // let screen = document.createElement("div");
 // gameScreenSection.appendChild(screen)
